4813f21275bdf0068df2e45b31a1908b4ad33484
Tarek Qassoumeh <144264754+Tarek-Qa@users.noreply.github.com>
2024-05-21 21:39:22 +0200

    Add project files.

---
+++ PixelPortalen.Infrastructure/DataAccess/Store/EventRepository.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Driver;
+using PixelPortalen.Domain.Entities;
+using PixelPortalen.Infrastructure.DataAccess.Store.Documents;
+
+namespace PixelPortalen.Infrastructure.DataAccess.Store;
+
+public class EventRepository : IEventRepository
+{
+    private readonly IMongoCollection<EventDocument> _eventCollection;
+
+    public EventRepository()
+    {
+        var hostName = "localhost";
+        var port = "27017";
+        var databaseName = "PixelPortalen";
+        var client = new MongoClient("mongodb://cosmosdbpixelportalen:GCBndbCjN3WMkaj7bHlQ0tPvE4BF1Pq0eTJw2NV6JI4h7DhJeG0EkMOLj8NY46WPjOVqUvHhS9aJACDbAbPpnQ==@cosmosdbpixelportalen.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@cosmosdbpixelportalen@");
+        var database = client.GetDatabase(databaseName);
+        _eventCollection = database.GetCollection<EventDocument>("EventCollection",
+            new MongoCollectionSettings() { AssignIdOnInsert = true });
+    }
+    public async Task AddAsync(EventDocument entity)
+    {
+        await _eventCollection.InsertOneAsync(entity);
+    }
+
+    public async Task<IEnumerable<EventDocument>> GetAllAsync()
+    {
+        var events = await _eventCollection.Find(t => true).ToListAsync();
+        return events;
+    }
+    public async Task UpdateAsync(EventDocument entity)
+    {
+        var filter = Builders<EventDocument>.Filter.Eq(s => s.Id, entity.Id);
+        await _eventCollection.ReplaceOneAsync(filter, entity);
+    }
+
+    public async Task AddUserToEvent(EventDocument entity)
+    {
+        var filter = Builders<EventDocument>.Filter.Eq(s => s.Id, entity.Id);
+        var update = Builders<EventDocument>.Update.Set(s => s.userIds, entity.userIds);
+        await _eventCollection.UpdateOneAsync(filter, update);
+    }
+    public async Task<EventDocument> GetByIdAsync(string id)
+    {
+        var eventDocument = await _eventCollection.Find(t => t.Id == id).FirstOrDefaultAsync();
+        return eventDocument;
+    }
+
+    public async Task<IEnumerable<EventDocument>> GetByUserIdAsync(string userId)
+    {
+        var events = await _eventCollection.Find(t => t.userIds.Contains(userId)).ToListAsync();
+        return events;
+    }
+    public async Task DeleteAsync(string id)
+    {
+        var filter = Builders<EventDocument>.Filter.Eq(s => s.Id, id);
+        await _eventCollection.DeleteOneAsync(filter);
+    }
+}
---
+++ PixelPortalen.Infrastructure/DataAccess/Store/ProductRepository.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Driver;
+using PixelPortalen.Infrastructure.DataAccess.Store.Documents;
+
+namespace PixelPortalen.Infrastructure.DataAccess.Store;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly IMongoCollection<ProductDocument> _productCollection;
+
+    public ProductRepository()
+    {
+        var hostName = "localhost";
+        var port = "27017";
+        var databaseName = "PixelPortalen";
+        var client = new MongoClient("mongodb://cosmosdbpixelportalen:GCBndbCjN3WMkaj7bHlQ0tPvE4BF1Pq0eTJw2NV6JI4h7DhJeG0EkMOLj8NY46WPjOVqUvHhS9aJACDbAbPpnQ==@cosmosdbpixelportalen.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@cosmosdbpixelportalen@");
+        var database = client.GetDatabase( databaseName );
+        _productCollection = database.GetCollection<ProductDocument>("ProductCollection",
+            new MongoCollectionSettings() { AssignIdOnInsert = true });
+    }
+
+    public async Task AddAsync(ProductDocument entity)
+    {
+        await _productCollection.InsertOneAsync(entity);
+    }
+
+    public async Task<IEnumerable<ProductDocument>> GetAllAsync()
+    {
+        var products = await _productCollection.Find(t => true).ToListAsync();
+        return products;
+    }
+
+    public async Task<ProductDocument> GetByEANAsync(long EAN)
+    {
+        var product = await _productCollection.Find(t => t.EAN == EAN).FirstOrDefaultAsync();
+        return product;
+    }
+
+    public async Task<ProductDocument> GetByIdAsync(string id)
+    {
+        var product = await _productCollection.Find(t => t.Id == id).FirstOrDefaultAsync();
+        return product;
+    }
+    public async Task UpdateReviewsAsync(ProductDocument entity)
+    {
+        var filter = Builders<ProductDocument>.Filter.Eq(s => s.Id, entity.Id);
+        var update = Builders<ProductDocument>.Update.Set(s => s.Reviews, entity.Reviews);
+        await _productCollection.UpdateOneAsync(filter, update);
+    }
+}
